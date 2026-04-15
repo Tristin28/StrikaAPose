6dd953066c5c06425fcdbd63c6a8b72a2d785af1
Tristin28 <tristin.bezzina.24@um.edu.mt>
2026-04-15 14:49:27 +0200

    Structuring the files

--- datapipeline/Build_CSV.py
+++ datapipeline/Build_CSV.py
@@ -2,35 +2,38 @@ import mediapipe as mp
 from new_features import normalize_and_extract_features
 import csv 
 
-model_path = "datapipeline/../Model/pose_landmarker_full.task"
+def creating_PoseLandmark_instance():
+  model_path = "datapipeline/../Model/pose_landmarker_full.task"
 
-'''
-    #Giving shorter names to the classes and enums that we will be using from the mediapipe library.
-'''
-BaseOptions = mp.tasks.BaseOptions #A class where its instance will have its fields initialised to the location (file path) of the model.
-PoseLandmarker = mp.tasks.vision.PoseLandmarker
-PoseLandmarkerOptions = mp.tasks.vision.PoseLandmarkerOptions
-VisionRunningMode = mp.tasks.vision.RunningMode #Enum for the running mode of the landmarker. It can be either IMAGE, VIDEO or LIVE_STREAM.
-
-'''
-    An instance which its fields are initialised with the respective configurations which come from other instances and enums.
-    Note further configurations can be added either later on or directly in the constructor of the PoseLandmarkerOptions class.
-'''
-options = PoseLandmarkerOptions(
-    base_options=BaseOptions(model_asset_path=model_path),
-    running_mode=VisionRunningMode.IMAGE,
-    num_poses=1
-)
-
-with PoseLandmarker.create_from_options(options) as landmarker:
   '''
-    Using a class method to create an instance of the PoseLandmarker - where this will provide method in order to interact with the pose pipeline
-    Then w.r.t with ... as ... the instance will be automatically freed from memory after the block of code is executed.
+      #Giving shorter names to the classes and enums that we will be using from the mediapipe library.
   '''
-  feature_list = normalize_and_extract_features("Images",landmarker)
+  BaseOptions = mp.tasks.BaseOptions #A class where its instance will have its fields initialised to the location (file path) of the model.
+  PoseLandmarker = mp.tasks.vision.PoseLandmarker
+  PoseLandmarkerOptions = mp.tasks.vision.PoseLandmarkerOptions
+  VisionRunningMode = mp.tasks.vision.RunningMode #Enum for the running mode of the landmarker. It can be either IMAGE, VIDEO or LIVE_STREAM.
 
-with open('../output.csv','w',newline='') as file:
-  writer=csv.writer(file)
-  for img_path, data in feature_list:
-    row = [img_path] + data.tolist()
-    writer.writerow(row)
+  '''
+      An instance which its fields are initialised with the respective configurations which come from other instances and enums.
+      Note further configurations can be added either later on or directly in the constructor of the PoseLandmarkerOptions class.
+  '''
+  options = PoseLandmarkerOptions(
+      base_options=BaseOptions(model_asset_path=model_path),
+      running_mode=VisionRunningMode.IMAGE,
+      num_poses=1
+  )
+
+  return PoseLandmarker.create_from_options(options)
+
+def save_features_to_csv(all_features,csv_path):
+  with open(csv_path,'w',newline='') as file:
+    writer=csv.writer(file)
+    for label, feature_vector in all_features:
+      row = [label] + feature_vector.tolist()
+      writer.writerow(row)
+
+if __name__ == "__main__":
+  with creating_PoseLandmark_instance() as landmarker:
+    feature_list = normalize_and_extract_features("Images",landmarker)
+  
+  save_features_to_csv(feature_list,"datapipeline/../output.csv")
--- datapipeline/new_features.py
+++ datapipeline/new_features.py
@@ -1,19 +1,22 @@
 import numpy as np
 from preprocessing_coords import extracting_raw_coords, normalise_single_pose
 
-LEFT_SHOULDER = 11
-RIGHT_SHOULDER = 12
-LEFT_ELBOW = 13
-RIGHT_ELBOW = 14
-LEFT_WRIST = 15
-RIGHT_WRIST = 16
-LEFT_HIP = 23
-RIGHT_HIP = 24
-LEFT_KNEE = 25
-RIGHT_KNEE = 26
-LEFT_ANKLE = 27
-RIGHT_ANKLE = 28
-NOSE = 0
+LANDMARKS = {
+    "LEFT_SHOULDER": 11,
+    "RIGHT_SHOULDER": 12,
+    "LEFT_ELBOW": 13,
+    "RIGHT_ELBOW": 14,
+    "LEFT_WRIST": 15,
+    "RIGHT_WRIST": 16,
+    "LEFT_HIP": 23,
+    "RIGHT_HIP": 24,
+    "LEFT_KNEE": 25,
+    "RIGHT_KNEE": 26,
+    "LEFT_ANKLE": 27,
+    "RIGHT_ANKLE": 28,
+    "NOSE": 0
+}
+
 
 def normalize_and_extract_features(images_folder, landmarker):
     extracted = extracting_raw_coords(images_folder, landmarker)
@@ -26,36 +29,36 @@ def normalize_and_extract_features(images_folder, landmarker):
             continue
 
         feature_vector = build_feature_vector(coords)
-        
-        all_features.append((file_path, feature_vector))
+        label = file_path.parent.name
+        all_features.append((label, feature_vector))
 
     return all_features
 
 def extract_joint_angles(coords):
     angles = [
-        compute_angle(coords[LEFT_SHOULDER], coords[LEFT_ELBOW], coords[LEFT_WRIST]),
-        compute_angle(coords[RIGHT_SHOULDER], coords[RIGHT_ELBOW], coords[RIGHT_WRIST]),
-        compute_angle(coords[LEFT_ELBOW], coords[LEFT_SHOULDER], coords[LEFT_HIP]),
-        compute_angle(coords[RIGHT_ELBOW], coords[RIGHT_SHOULDER], coords[RIGHT_HIP]),
-        compute_angle(coords[LEFT_SHOULDER], coords[LEFT_HIP], coords[LEFT_KNEE]),
-        compute_angle(coords[RIGHT_SHOULDER], coords[RIGHT_HIP], coords[RIGHT_KNEE]),
-        compute_angle(coords[LEFT_HIP], coords[LEFT_KNEE], coords[LEFT_ANKLE]),
-        compute_angle(coords[RIGHT_HIP], coords[RIGHT_KNEE], coords[RIGHT_ANKLE]),
+        compute_angle(coords[LANDMARKS["LEFT_SHOULDER"]], coords[LANDMARKS["LEFT_ELBOW"]], coords[LANDMARKS["LEFT_WRIST"]]),
+        compute_angle(coords[LANDMARKS["RIGHT_SHOULDER"]], coords[LANDMARKS["RIGHT_ELBOW"]], coords[LANDMARKS["RIGHT_WRIST"]]),
+        compute_angle(coords[LANDMARKS["LEFT_ELBOW"]], coords[LANDMARKS["LEFT_SHOULDER"]], coords[LANDMARKS["LEFT_HIP"]]),
+        compute_angle(coords[LANDMARKS["RIGHT_ELBOW"]], coords[LANDMARKS["RIGHT_SHOULDER"]], coords[LANDMARKS["RIGHT_HIP"]]),
+        compute_angle(coords[LANDMARKS["LEFT_SHOULDER"]], coords[LANDMARKS["LEFT_HIP"]], coords[LANDMARKS["LEFT_KNEE"]]),
+        compute_angle(coords[LANDMARKS["RIGHT_SHOULDER"]], coords[LANDMARKS["RIGHT_HIP"]], coords[LANDMARKS["RIGHT_KNEE"]]),
+        compute_angle(coords[LANDMARKS["LEFT_HIP"]], coords[LANDMARKS["LEFT_KNEE"]], coords[LANDMARKS["LEFT_ANKLE"]]),
+        compute_angle(coords[LANDMARKS["RIGHT_HIP"]], coords[LANDMARKS["RIGHT_KNEE"]], coords[LANDMARKS["RIGHT_ANKLE"]]),
     ]
 
     return np.array(angles, dtype=np.float64)
 
 def extract_key_distances(coords):
-    shoulder_center = (coords[LEFT_SHOULDER] + coords[RIGHT_SHOULDER]) / 2
+    shoulder_center = (coords[LANDMARKS["LEFT_SHOULDER"]] + coords[LANDMARKS["RIGHT_SHOULDER"]]) / 2
 
     distances = [
-        np.linalg.norm(coords[LEFT_WRIST] - coords[RIGHT_WRIST]),
-        np.linalg.norm(coords[LEFT_WRIST] - coords[NOSE]),
-        np.linalg.norm(coords[RIGHT_WRIST] - coords[NOSE]),
-        np.linalg.norm(coords[LEFT_WRIST] - shoulder_center),
-        np.linalg.norm(coords[RIGHT_WRIST] - shoulder_center),
-        np.linalg.norm(coords[LEFT_ANKLE] - shoulder_center),
-        np.linalg.norm(coords[RIGHT_ANKLE] - shoulder_center),
+        np.linalg.norm(coords[LANDMARKS["LEFT_WRIST"]] - coords[LANDMARKS["RIGHT_WRIST"]]),
+        np.linalg.norm(coords[LANDMARKS["LEFT_WRIST"]] - coords[LANDMARKS["NOSE"]]),
+        np.linalg.norm(coords[LANDMARKS["RIGHT_WRIST"]] - coords[LANDMARKS["NOSE"]]),
+        np.linalg.norm(coords[LANDMARKS["LEFT_WRIST"]] - shoulder_center),
+        np.linalg.norm(coords[LANDMARKS["RIGHT_WRIST"]] - shoulder_center),
+        np.linalg.norm(coords[LANDMARKS["LEFT_ANKLE"]] - shoulder_center),
+        np.linalg.norm(coords[LANDMARKS["RIGHT_ANKLE"]] - shoulder_center),
     ]
 
     return np.array(distances, dtype=np.float64)
